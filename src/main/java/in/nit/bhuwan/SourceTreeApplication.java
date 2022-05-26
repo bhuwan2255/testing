040d80a67238a0952d267e5e3cb8086c1197a705
Bhuwan <Bhuwanhome@gmail.com>
2022-05-26 21:45:51 +0530

    first commit

---
+++ src/main/java/in/nit/bhuwan/SourceTreeApplication.java
@@ -0,0 +1,13 @@
+package in.nit.bhuwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nit/bhuwan/SourceTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nit.bhuwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
